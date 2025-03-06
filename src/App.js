f7a5f0bc8173e7513ca31e6d226aa821565b4214
blackjacked937 <villegaschristian514@gmail.com>
2025-03-06 00:23:27 -0600

    Actualización de Navbar y App.js para GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import OrdenesDeCompra from "./Components/Ordenes de compra/OrdenesDeCompraView";
 import PerfilUsuario from "./Components/Perfil/PerfilUsuarioView";
 import ListaPedidos from "./Components/Lista Pedidos/ListaPedidosView";
@@ -7,18 +7,16 @@ import Navbar from "./Components/Navbar";
 import Home from "./Components/Home/HomeView";
 import ProductosView from "./Components/Productos/ProductosView";
 
-
 const App = () => {
   return (
     <Router>
       <Navbar />
       <Routes>
-      <Route path="/home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/ordenes" element={<OrdenesDeCompra />} />
         <Route path="/lista_pedidos" element={<ListaPedidos />} />
         <Route path="/productos" element={<ProductosView />} />
         <Route path="/perfil" element={<PerfilUsuario />} />
-  
       </Routes>
     </Router>
   );
