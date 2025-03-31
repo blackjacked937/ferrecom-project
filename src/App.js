a708171b60258e51759ce5fd486fe45f8500c52f
blackjacked937 <villegaschristian514@gmail.com>
2025-03-31 00:13:48 -0600

    Archivos corregidos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import OrdenesDeCompra from "./Components/Ordenes de compra/OrdenesDeCompraView";
 import PerfilUsuario from "./Components/Perfil/PerfilUsuarioView";
 import ListaPedidos from "./Components/Lista Pedidos/ListaPedidosView";
